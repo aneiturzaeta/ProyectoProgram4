a6bc3372c4183033e9a38388d21baf7ca74d2434
gorkaotba <gorkaotba@opendeusto.es>
2016-04-12 20:55:47 +0200

    Actualizar menu principal

--- menuPrincipal.h
+++ menuPrincipal.h
@@ -1,36 +1,12 @@
 #ifndef _MENUPRINCIPAL_H_
 #define _MENUPRINCIPAL_H_
 
-typedef struct {
-	int matricula;
-	char nombre[25]; 
-} Trabajador;
 
 
-void actualizar_plazas( int estado[], int tope_plazas);
+void actualizar_plazas( int estado[], int topeplazas);
 
 
 void menuInicial(void);
 
-
-int identificarse(void);
-	
-
-void aparcar(int indentU);
-
-
-void sacarCoche(int opcion);
-
-
-void menuUsuario(void);
-
-
-void cambiarContrasena(void);
-
-
-void comprobarContrasena(void);
-
-
-void menuAdministrador(void);
 
 #endif
